56807db6b92bc5e004f40f52502fc829cf963d65
rwHelgeson <foo@bar>
2021-01-20 07:20:10 -0600

    Initial Commit

---
+++ qz/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  qz
+//
+//  Created by y3h on 2021/01/20.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+   // insert code here...
+   std::cout << "Hello, World!\n";
+   return 0;
+}
